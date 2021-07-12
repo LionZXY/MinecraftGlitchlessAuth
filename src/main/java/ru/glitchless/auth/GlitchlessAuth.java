2a8d42dc610aceda4c36797d2a20427665f7e875
Nikita Kulikov <nkulikov@snapchat.com>
2021-07-12 21:24:52 +0300

    Add 1.7.10

--- src/main/java/ru/glitchless/auth/GlitchlessAuth.java
+++ src/main/java/ru/glitchless/auth/GlitchlessAuth.java
@@ -2,30 +2,27 @@ package ru.glitchless.auth;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import okhttp3.OkHttpClient;
 import org.apache.logging.log4j.Logger;
-import ru.glitchless.auth.loopers.MainLooper;
-import ru.glitchless.auth.loopers.UpdateLooper;
+import ru.glitchless.auth.utils.ReplaceServerHelper;
 
-@Mod(modid = GlitchlessAuth.MODID, name = GlitchlessAuth.NAME, version = GlitchlessAuth.VERSION,
-        acceptableRemoteVersions = "*")
+@Mod(
+        modid = GlitchlessAuth.MODID,
+        name = GlitchlessAuth.NAME,
+        version = "${version}",
+        acceptableRemoteVersions = "*"
+)
 public class GlitchlessAuth {
     public static final String MODID = "glitchlessauth";
     public static final String NAME = "Glitchless Auth Control";
-    public static final String VERSION = "1.0";
 
     private static Logger logger;
     private static GlitchlessAuth INSTANCE;
     private static final Gson gson = new GsonBuilder().create();
     private static final OkHttpClient client = new OkHttpClient.Builder().build();
-
-    private MainLooper mainLooper = new MainLooper();
-    private UpdateLooper updateLooper = new UpdateLooper();
 
     public GlitchlessAuth() {
         this.INSTANCE = this;
@@ -42,21 +39,16 @@ public class GlitchlessAuth {
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-
-        MinecraftForge.EVENT_BUS.register(mainLooper);
-    }
-
-    @Mod.EventHandler
-    public void serverStarting(FMLServerStartingEvent event) {
-        updateLooper.start();
+        try {
+            new ReplaceServerHelper().replaceAll();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new RuntimeException("I can't replace auth urls", ex);
+        }
     }
 
     public static GlitchlessAuth getInstance() {
         return INSTANCE;
-    }
-
-    public MainLooper getMainLooper() {
-        return mainLooper;
     }
 
     public static Gson getGson() {
@@ -65,9 +57,5 @@ public class GlitchlessAuth {
 
     public static OkHttpClient getClient() {
         return client;
-    }
-
-    public UpdateLooper getUpdateLooper() {
-        return updateLooper;
     }
 }
---
+++ src/main/java/ru/glitchless/auth/utils/ReplaceServerHelper.java
@@ -0,0 +1,61 @@
+package ru.glitchless.auth.utils;
+
+import com.mojang.authlib.HttpAuthenticationService;
+import com.mojang.authlib.yggdrasil.YggdrasilMinecraftSessionService;
+import com.mojang.authlib.yggdrasil.YggdrasilUserAuthentication;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+public class ReplaceServerHelper {
+    private static final String BASE_URL = "https://games.glitchless.ru/api/minecraft/users/";
+
+    static void setFinalStatic(Field field, Object newValue) throws Exception {
+        field.setAccessible(true);
+
+        Field modifiersField = Field.class.getDeclaredField("modifiers");
+        modifiersField.setAccessible(true);
+        modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+
+        field.set(null, newValue);
+    }
+
+    public void replaceAll() throws Exception {
+        replaceSession();
+        replaceAuthServer();
+    }
+
+    private void replaceSession() throws Exception {
+        Field baseUrl = YggdrasilMinecraftSessionService.class.getDeclaredField("BASE_URL");
+        Field joinUrl = YggdrasilMinecraftSessionService.class.getDeclaredField("JOIN_URL");
+        Field checkUrl = YggdrasilMinecraftSessionService.class.getDeclaredField("CHECK_URL");
+        baseUrl.setAccessible(true);
+        joinUrl.setAccessible(true);
+        checkUrl.setAccessible(true);
+        setFinalStatic(baseUrl, BASE_URL);
+        setFinalStatic(joinUrl, HttpAuthenticationService.constantURL(BASE_URL + "join"));
+        setFinalStatic(checkUrl, HttpAuthenticationService.constantURL(BASE_URL + "has_joined"));
+    }
+
+    private void replaceAuthServer() throws Exception {
+        Field baseUrl = YggdrasilUserAuthentication.class.getDeclaredField("BASE_URL");
+        Field routeAuthenticate = YggdrasilUserAuthentication.class.getDeclaredField("ROUTE_AUTHENTICATE");
+        Field routeRefresh = YggdrasilUserAuthentication.class.getDeclaredField("ROUTE_REFRESH");
+        Field routeValidate = YggdrasilUserAuthentication.class.getDeclaredField("ROUTE_VALIDATE");
+        Field routeInvalidate = YggdrasilUserAuthentication.class.getDeclaredField("ROUTE_INVALIDATE");
+        Field routeSignout = YggdrasilUserAuthentication.class.getDeclaredField("ROUTE_SIGNOUT");
+        baseUrl.setAccessible(true);
+        routeAuthenticate.setAccessible(true);
+        routeRefresh.setAccessible(true);
+        routeValidate.setAccessible(true);
+        routeInvalidate.setAccessible(true);
+        routeSignout.setAccessible(true);
+
+        setFinalStatic(baseUrl, BASE_URL);
+        setFinalStatic(routeAuthenticate, HttpAuthenticationService.constantURL(BASE_URL + "authenticate"));
+        setFinalStatic(routeRefresh, HttpAuthenticationService.constantURL(BASE_URL + "refresh"));
+        setFinalStatic(routeValidate, HttpAuthenticationService.constantURL(BASE_URL + "validate"));
+        setFinalStatic(routeInvalidate, HttpAuthenticationService.constantURL(BASE_URL + "invalidate"));
+        setFinalStatic(routeSignout, HttpAuthenticationService.constantURL(BASE_URL + "signout"));
+    }
+}
